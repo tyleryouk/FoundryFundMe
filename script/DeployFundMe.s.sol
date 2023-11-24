049440d75719ceb4a7a32a24ae2c572a3aa7b9ce
tyleryouk <tyleryouk@gmail.com>
2023-11-23 23:48:48 -0500

    created a mock tester to mimic the behavior of a price aggregator contract (price aggregators are commonly used in DeFi applications)

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPricefeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0xCfF999A68A2BA78412f3Be6bd155B9DAB5138D99);
+        FundMe fundMe = new FundMe(ethUsdPricefeed);
         vm.stopBroadcast();
         return fundMe;
     }
